0da963ea2be843a08f73afad59db571659487ec5
Maxwell Zen <maxwell.zen@gmail.com>
2021-11-24 10:40:58 -0500

    added unbuffered input

--- main.c
+++ main.c
@@ -15,26 +15,33 @@
 int main() {
 	char line[1000];
 	char **parsed;
+	int i = 0;
+	char c;
+	printf("Enter command: ");
+	fflush(stdout);
 
 	while(1) {
-		printf("Enter command: ");
-		fflush(stdout);
+		c = getchar();
+		if (c=='\n') {
+			// split input -- take line and create string array, splitting by space
+	       	parsed = split(line);
 
-		// gather input -- after user presses newline, string goes into line
-		input(line);
-
-		// split input -- take line and create string array, splitting by space
-       	parsed = split(line);
-
-		char **cur, **prev;
-		for (cur = prev = parsed; *cur; cur++) {
-			if (strcmp(*cur, ";")==0) {
-				*cur = 0;
-				eval(prev);
-				prev = cur+1;
+			char **cur, **prev;
+			for (cur = prev = parsed; *cur; cur++) {
+				if (strcmp(*cur, ";")==0) {
+					*cur = 0;
+					eval(prev);
+					prev = cur+1;
+				}
 			}
+			eval(prev);
+			printf("Enter command: ");
+			fflush(stdout);
+			i = 0;
+		} else {
+			line[i++]=c;
+			line[i]=0;
 		}
-		eval(prev);
 
 	}
 
