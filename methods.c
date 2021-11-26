2d059327f4b92a201d3857d8e7f4dd81d447ac13
jennisun <jsun20@stuy.edu>
2021-11-26 10:17:07 -0500

    commented out old parsing code

--- methods.c
+++ methods.c
@@ -11,13 +11,6 @@
 #include <errno.h>
 #include "methods.h"
 
-// takes string, reads input into the string
-void input(char * c) {
-	fgets(c, 1000, stdin);
-	int n = strlen(c);
-	if (c[n-1]=='\n') c[n-1]=0;
-}
-
 // takes string, returns parsed input
 char** split(char * c) {
   int i = 0;
@@ -44,11 +37,27 @@ char** split(char * c) {
 
   char ** args = calloc(size, sizeof(char *));
   for (int i = 0; i < size; i ++) {
-    args[i] = strsep(&c, " ");
-    // printf("%s\n", args[i]);
+    args[i] = strsep(&d, " ");
   }
   return args;
 }
+
+// char** split(char * c) {
+//   int i = 0;
+//   int size = 1;
+//
+//   while (c[i] != '\0') {
+//     if (c[i] == ' ') size += 1;
+//     i += 1;
+//   }
+//
+//   char ** args = calloc(size, sizeof(char *));
+//   for (int i = 0; i < size; i ++) {
+//     args[i] = strsep(&c, " ");
+//     // printf("%s\n", args[i]);
+//   }
+//   return args;
+// }
 
 // takes arguments, executes commands
 void eval(char **parsed) {
